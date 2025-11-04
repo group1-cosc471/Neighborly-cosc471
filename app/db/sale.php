e4e777344a8434369a7b18dbafa3dca927abc549
William Dalian <williamdalian1@gmail.com>
2025-11-04 09:21:28 -0500

    Update sale.php
    Add functions to get All Sales, sales by sale_D, and Sales by seller_id.

--- app/db/sale.php
+++ app/db/sale.php
@@ -1,2 +1,50 @@
 //William Dalian
-?<php
+<?php
+$sale_id
+$seller_id
+$sale_street_address
+$sale_municipality
+
+ 
+//get sales
+function getAllSales(){
+    $sales_list = []
+    $query -> (
+        'SELECT Sale_id, seller_id, Sale_street_address, Sale_municipality
+        FROM SALE'
+    ) ;
+
+    while ($result -> $query.fetchRow()){
+        sales_list[] = [$result];
+    }
+
+    return $sales_list;
+}
+
+//get sale by sale id
+function getSale($id){
+    $query -> (
+        'SELECT *
+        FROM SALE
+        WHERE Sale_id = $id'
+    ) ;
+
+    return $result - >$query.fetchRow();
+}
+
+//get sales by seller_id
+function getSalesBySeller($s_id){
+    $sales_lst = []
+    $query -> (
+        'SELECT *
+        FROM SALE
+        WHERE Seller_id = $s_id'
+    );
+
+    while ($result -> $query.fetchRow()){
+        sales_list[] = [$result];
+    }
+
+    return $sales_list;
+}
+?>
