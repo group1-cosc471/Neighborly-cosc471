ecb5f71630e342d2800939c06a16218e11ad34b7
Jason Reaume <jreaume5@emich.edu>
2025-11-06 17:17:16 -0500

    Added function that gets a user's credentials based on their username (email address).

--- app/db/user.php
+++ app/db/user.php
@@ -1,11 +1,50 @@
 <?php
-//Jason 
- require_once 'database.php';
+// Jason R.
 
-//needs to return array with username and password
- function getUserLogin($credentials) {
-    $user = [];
-    
-    return $user;
- }
-?>
+// Establish the database connection
+require_once 'database.php';
+
+// Declare variables for relevant user data
+$user_id;
+$email;
+$password;
+$first_name;
+$last_name;
+$phone_number;
+
+// Returns a list containing the associated id, username, and password 
+// given a user's credentials
+function getUserLogin($conn, $email)
+{
+   # Array containing the user's username (email), password, and user id
+   $user = ['username' => $email, 'password' => '', 'u_id' => ''];
+
+   $query = $conn->prepare(
+      "SELECT u_id, user_password
+      FROM user
+      WHERE email = ?"
+   );
+   // Bind the email parameter to the ? placeholder for email in the
+   // query/statement
+   $query->bind_param("s", $email);
+
+   // Execute the query/statement
+   $query->execute();
+
+   // Get the result of the executed query/statement
+   $result = $query->get_result();
+
+   // Store the username, user id, and password if the user exists (user
+   // entered a valid email)
+   if ($row = $result->fetch_assoc()) {
+      $user['password'] = $row['user_password'];
+      $user['u_id'] = $row['u_id'];
+   } else {
+      // No user found
+      echo ("No user found with email: " . $email);
+      return null;
+   }
+
+   // Return the user's credentials
+   return $user;
+}
