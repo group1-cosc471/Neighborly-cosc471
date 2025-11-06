d5a5c7cf2104e48b09ba72e166e32722a0c107d7
Jason Reaume <jreaume5@emich.edu>
2025-11-06 17:43:03 -0500

    Addeed function 'createUser' that will be called whenever a user creates a new account with the website. Currently does not account for invalid inputs.

--- app/db/user.php
+++ app/db/user.php
@@ -4,16 +4,38 @@
 // Establish the database connection
 require_once 'database.php';
 
-// Declare variables for relevant user data
-$user_id;
-$email;
-$password;
-$first_name;
-$last_name;
-$phone_number;
+// This function is called when a user decides to create an account on
+// the website.
+function createUser($conn, $username, $password, $first_name, $last_name, $phone_number)
+{
+   // Use the default PHP one-way hashing algorithm (bcrypt) to create a 
+   // hashed password.
+   $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Returns a list containing the associated id, username, and password 
-// given a user's credentials
+   // Prepare the query for execution
+   $query = $conn->prepare(
+      "INSERT INTO user (email, user_password, first_name, last_name, phone_number)
+      VALUES (?, ?, ?, ?, ?, ?)"
+   );
+
+   // Bind query parameters
+   $query->bind_param("ssssss", $username, $hashed_password, $first_name, $last_name, $phone_number);
+
+   // Execute the query
+   if ($query->execute()) {
+      // Upon the successful creation of an account, insert the auto
+      // incremented ID of the newly inserted user.
+      $user_id = $conn->insert_id;
+      echo "New user was created successfully with ID: " . $user_id;
+      return $user_id;
+   } else {
+      echo "An error was encountered while creating a new user: " . $query->error;
+      return false;
+   }
+}
+
+// Returns a list containing the associated ID, username, and password 
+// given a user's credentials.
 function getUserLogin($conn, $email)
 {
    # Array containing the user's username (email), password, and user id
@@ -24,6 +46,7 @@ function getUserLogin($conn, $email)
       FROM user
       WHERE email = ?"
    );
+
    // Bind the email parameter to the ? placeholder for email in the
    // query/statement
    $query->bind_param("s", $email);
