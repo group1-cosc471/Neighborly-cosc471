13df35f3c1ded1bffe54788dfb275346ee1bb249
pkmartus <pkmartus@gmail.com>
2025-11-10 11:43:33 -0500

    started work on list sales. currently only lists location

--- app/db/items.php
+++ app/db/items.php
@@ -2,4 +2,10 @@
 <?php
 
 require_once __DIR__ . '/database.php';
+
+//return the number of items in a sale based on sale id
+function itemsInSale($saleID) {
+
+    return 1;
+}
 ?>
--- pages/listsales.php
+++ pages/listsales.php
@@ -7,10 +7,28 @@ function init()
     $acknowlegement = '';
     require_once '../app/db/sale.php';
 
+    $sales = getAllSales();
+
+    //start the list with the titles
     $list = <<<HTML
          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
          <h1>All Sales</h1>
+         <table class="table">
+            <tr>
+                <th>Location</th>
+                <th>Host</th>
+                <th>Number of items</th>
+            </tr>
     HTML;
+
+    //append each sale from the database into the table
+    foreach($sales as $sale) {
+        $list .= "<tr>
+                <td>{$sale['street_address']}, {$sale['municipality']}</td>
+                </tr>";//todo add seller and number of items, make sale location a link to the sale page
+    }
+
+    $list .= "</table>";
 
     return [$acknowlegement, $list];
 }
