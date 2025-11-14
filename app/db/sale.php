4032f080d57af4a91db95d50b739a5ab15759619
pkmartus <pkmartus@gmail.com>
2025-11-14 10:27:20 -0500

    fixed small errors

--- app/db/sale.php
+++ app/db/sale.php
@@ -9,20 +9,21 @@ $seller_id;
 $sale_street_address;
 $sale_municipality;
 
- 
+
 //get sales
-function getAllSales(){
+function getAllSales()
+{
     //I guess this is required for use inside php functions, my last project just used individual files so my bad
     global $conn;
     $sales_list = [];
 
     //syntax for defining query requires the connecttion to the database
-    $query = 
+    $query =
         'SELECT sale_id, seller_id, street_address, municipality
         FROM sale';
     $result = $conn->query($query);
 
-    while ($row = $result->fetch_assoc()){ //use to get row from result
+    while ($row = $result->fetch_assoc()) { //use to get row from result
         //append to an array like this othe
         $sales_list[] = $row; //append row to list
     }
@@ -31,21 +32,23 @@ function getAllSales(){
 }
 
 //get sale by sale id
-function getSale($id){
+function getSale($id)
+{
     global $conn;
     $query =  $conn->prepare(
         'SELECT *
         FROM sale
         WHERE sale_id = ? '
     );
-    $query ->bind_param("i", $id);
+    $query->bind_param("i", $id);
     $result = $query->get_result();
 
     return $result->fetch_assoc();
 }
 
 //get sales by seller_id
-function getSalesBySeller($s_id){
+function getSalesBySeller($s_id)
+{
     //syntax for prepared statements is correct in this function. apply to other functions
     global $conn;
     $sales_list = [];
@@ -57,9 +60,9 @@ function getSalesBySeller($s_id){
     $query->execute();
 
     $result = $query->get_result();
-    
 
-    while ($row =  $result->fetch_assoc()){
+
+    while ($row =  $result->fetch_assoc()) {
         $sales_list[] = $row;
     }
 
@@ -69,29 +72,26 @@ function getSalesBySeller($s_id){
 //posts sale data to the database. Usese session variables
 //to get seller id. Returns 0 for success, 1 for database
 //posting failue, 2 for if the user is not logged in
-function postSale( $streetAddress, $municipality){
-    $seller_id;
+function postSale($streetAddress, $municipality)
+{
+    // $seller_id;
 
     //check if a session id is set (user logged in)
-    if (isset($_SESSION['user'])){
-        $seller_id = $_SESSION['user'] 
+    if (isset($_SESSION['user'])) {
+        $seller_id = $_SESSION['user'];
 
         global $conn;
         $statement =  $conn->prepare(
             'INSERT INTO sale (seller_id, street_address, municipality) VALUES (?, ?, ?)'
         );
-        $statement ->bind_param("iss", $seller_id, $streetAddress, $municipality);
-        
-        if($statement -> execute()){
-            return 0;
-        }
+        $statement->bind_param("iss", $seller_id, $streetAddress, $municipality);
 
-        else{
+        if ($statement->execute()) {
+            return 0;
+        } else {
             return 1;
-            }    
-    else {
-        return 2;
         }
-    }    
+    } else {
+        return 2;
+    }
 }
-?>
