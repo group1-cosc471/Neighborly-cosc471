eb5b2c8991ee6ff0baf58438c8e2857bf4ba944e
pkmartus <pkmartus@gmail.com>
2025-11-16 10:27:46 -0500

    fixed missing } lost when resolving merge

--- pages/createsales.php
+++ pages/createsales.php
@@ -5,7 +5,7 @@ function init()
 {
     require_once '../app/db/sale.php';
     $message = "";
-      
+
     $street =  "";
     $municipality =  "";
     $s_date =  "";
@@ -60,32 +60,33 @@ function init()
         </body>
         HTML;
 
-    
-if (isset($_POST['post-sale'])){
-    $street =  htmlspecialchars($_POST['streetAddr'] ?? "");
-    $municipality =  htmlspecialchars($_POST['municipality'] ?? "");
-    $s_date =  htmlspecialchars($_POST['start_date'] ?? "");
-    $e_date =  htmlspecialchars($_POST['end_date'] ?? "");
-    $open_time =  htmlspecialchars($_POST['open_time'] ?? "");
-    $close_time =  htmlspecialchars($_POST['close_time'] ?? "");
-    $sale_type =  htmlspecialchars($_POST['sale_type'] ?? "");
 
-    //call to postSale function, pass user submitted values, and get result
-    $result = postSale($street, $municipality, $s_date, $e_date, $open_time, $close_time, $sale_type);
+    if (isset($_POST['post-sale'])) {
+        $street =  htmlspecialchars($_POST['streetAddr'] ?? "");
+        $municipality =  htmlspecialchars($_POST['municipality'] ?? "");
+        $s_date =  htmlspecialchars($_POST['start_date'] ?? "");
+        $e_date =  htmlspecialchars($_POST['end_date'] ?? "");
+        $open_time =  htmlspecialchars($_POST['open_time'] ?? "");
+        $close_time =  htmlspecialchars($_POST['close_time'] ?? "");
+        $sale_type =  htmlspecialchars($_POST['sale_type'] ?? "");
 
-    //if success, display a message stating such
-    if ($result == 0){
-        $message = "Succesfully posted sale. Thank you.";
+        //call to postSale function, pass user submitted values, and get result
+        $result = postSale($street, $municipality, $s_date, $e_date, $open_time, $close_time, $sale_type);
+
+        //if success, display a message stating such
+        if ($result == 0) {
+            $message = "Succesfully posted sale. Thank you.";
+        }
+
+        //if theres an error creating the sale, show a message stating such.
+        if ($result == 1) {
+            $message = "Error creating sale. Please try again.";
+        }
+
+        //if the user is not logged it, state that the user must login.
+        if ($result == 2) {
+            $message = "Error: you must be logged in to create a sale.";
+        }
+        return [$message, $form];
     }
-
-    //if theres an error creating the sale, show a message stating such.
-    if ($result == 1){
-        $message = "Error creating sale. Please try again.";
-    }
-
-    //if the user is not logged it, state that the user must login.
-    if ($result == 2){
-        $message = "Error: you must be logged in to create a sale.";
-    }
-    return [$message, $form];
 }
