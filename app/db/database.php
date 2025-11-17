ceab1adcce3cb18b1d299e9d6b52e4053de2190e
Jason Reaume <jreaume5@emich.edu>
2025-11-17 13:32:14 -0500

    Update database.php and listsales.php for compatability with changes in jason/user

--- app/db/database.php
+++ app/db/database.php
@@ -9,10 +9,28 @@ $dbname = "neighborly_lol";
 
 //create the connection to the database
 global $conn;
-$conn = new mysqli($servername, $username, $password, $dbname);
-//check db connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
-?>
+// Try connecting to MySQL database via localhost
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check if db connection via localhost failed
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+    // Handle exception connecting to db via localhost
+} catch (Exception $e) {
+    // Upon failure connecting to db via localhost, try connecting via 
+    // 127.0.0.1
+    try {
+        $servername = "127.0.0.1";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check if connection via 127.0.0.1 failed
+        if ($conn->connect_error) {
+            throw new Exception("Connection failed: " . $conn->connect_error);
+        }
+        // Handle exception connecting to db via 127.0.0.1
+    } catch (Exception $e2) {
+        die("Connection failed: " . $e2->getMessage());
+    }
+}
